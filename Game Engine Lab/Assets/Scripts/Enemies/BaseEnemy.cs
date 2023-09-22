33d543629ae04e3ba6d377b9dcc0c72dd1336201
joshMorgan06 <joshua.morgan3@ontariotechu.net>
2023-09-22 15:23:55 -0400

    First Lab Stuff
    Unity just isn't compiling my scripts but the code is there :/ I'll look at lab slides later and try to get it to work

---
+++ Game Engine Lab/Assets/Scripts/Enemies/BaseEnemy.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseEnemy : MonoBehaviour
+{
+    [SerializeField]
+    protected int attackStat = 1;
+
+    int m_currentHealth;
+    bool m_isDead;
+
+    [SerializeField]
+    protected float moveSpeed = 1.0f;
+    [SerializeField]
+    protected float baseHealth = 10.0f;
+
+    [SerializeField]
+    protected Vector3 colour = new Vector3(1.0f, 1.0f, 1.0f);
+
+    Collider enemyCollider = null;
+
+    EnemyState enemyState = EnemyState.idle;
+
+    public enum EnemyState
+    {
+        idle,
+        wander,
+        attack,
+    }
+
+    // Start is called before the first frame update
+    public virtual void Start()
+    {
+        enemyCollider = this.transform.GetComponent<Collider>();
+    }
+
+    // Update is called once per frame
+    public virtual void Update()
+    {
+        Move();
+    }
+
+    public virtual void Attacking()
+    {
+        enemyCollider.enabled = true;
+        enemyState = EnemyState.attack;
+    }
+
+    public virtual void Move()
+    {
+        this.transform.position = this.transform.position + new Vector3(0.0f, 0.0f, 1.0f) * moveSpeed * Time.deltaTime;
+    }
+}
---
+++ Game Engine Lab/Assets/Scripts/Enemies/Goomba.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goomba : BaseEnemy
+{
+    [SerializeField]
+    protected int defense = 1;
+
+    // Start is called before the first frame update
+    public override void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public override void Update()
+    {
+        
+    }
+
+    // Override for attacking
+    public override void Attacking()
+    {
+        // Does nothing
+    }
+}
